dfd531e1b0b8914719ea50190e9eee9923ff0bb6
lucker2002 <81064375@qq.com>
2018-12-26 20:13:25 +0800

    Update NativeUITools.js

--- www/NativeUITools.js
+++ www/NativeUITools.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'cordova-plugin-nativeUITools', 'coolMethod', [arg0]);
+exports.NativeUITools = function (arg0, success, error) {
+    exec(success, error, 'cordova-plugin-nativeUITools', 'NativeUITools', [arg0]);
 };
