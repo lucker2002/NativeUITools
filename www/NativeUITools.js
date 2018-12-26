cbd0ab22085088194720792307cac9d12980dac4
lucker2002 <81064375@qq.com>
2018-12-26 20:22:01 +0800

    Update NativeUITools.js

--- www/NativeUITools.js
+++ www/NativeUITools.js
@@ -1,5 +1,26 @@
 var exec = require('cordova/exec');
 
-exports.NativeUITools = function (arg0, success, error) {
-    exec(success, error, 'cordova-plugin-nativeUITools', 'NativeUITools', [arg0]);
-};
+let NativeUITools = {
+
+    //获取刘海高度
+    getNotchHeight:function (arg0, success, error) {   
+        exec(success, error, 'NativeUITools', 'getNotchHeight', []);
+    },
+
+    //设置沉浸
+    setImmerse: function (arg0, success, error) {
+        exec(success, error, 'NativeUITools', 'setImmerse', []);
+    },
+
+    //获取虚拟按键高度
+    getNavigationBarHeight:function (arg0, success, error) {
+        exec(success, error, 'NativeUITools', 'getNavigationBarHeight', []);
+    },
+
+    //设置全屏
+    setFullScreen:function (arg0, success, error) {
+        exec(success, error, 'NativeUITools', 'setFullScreen', []);
+    },
+    
+}
+module.exports = NativeUITools;
